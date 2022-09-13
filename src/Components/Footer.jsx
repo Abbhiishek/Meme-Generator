5364f5d4f960f522b65028e506d2e0c98363384d
Abbhiishek <abhishekkushwaha1479@gmail.com>
2022-09-13 21:08:38 +0530

    Footer data

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <>
+      <footer className="footer bg-black px-10 text-white pt-12 pb-10 text-center   w-full">
+       <h1 className='font-mono font-bold'>Made with ❤️ React Vite by Abhishek Kushwaha</h1>
+      </footer>
+
+    </>
+  )
+}
